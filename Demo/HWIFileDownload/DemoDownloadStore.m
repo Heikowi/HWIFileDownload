2b335a540ec6d3f092fa9305b53971d7b21192aa
Heiko Wichmann <heiko@imagomat.de>
2016-03-05 10:38:53 +0100

    Test data

--- Demo/HWIFileDownload/DemoDownloadStore.m
+++ Demo/HWIFileDownload/DemoDownloadStore.m
@@ -97,7 +97,7 @@ static void *DemoDownloadStoreProgressObserverContext = &DemoDownloadStoreProgre
         if (aFoundDownloadItemIndex == NSNotFound)
         {
             NSURL *aRemoteURL = [NSURL URLWithString:[NSString stringWithFormat:@"http://www.imagomat.de/testimages/%@.tiff", @(aDownloadIdentifierUInteger)]];
-            if ([aDownloadIdentifier isEqualToString:@"3"])
+            if ([aDownloadIdentifier isEqualToString:@"5"])
             {
                 aRemoteURL = [NSURL URLWithString:@"http://www.imagomat.de/testimages/900.tiff"];
             }
