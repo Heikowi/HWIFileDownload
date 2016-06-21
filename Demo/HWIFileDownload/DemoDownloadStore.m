cce3bce2ae30fc62bddf3bc724e10ed5d80519ab
Heiko Wichmann <heiko@imagomat.de>
2016-06-21 20:39:11 +0200

    Sample code for customized http headers

--- Demo/HWIFileDownload/DemoDownloadStore.m
+++ Demo/HWIFileDownload/DemoDownloadStore.m
@@ -395,6 +395,17 @@ static void *DemoDownloadStoreProgressObserverContext = &DemoDownloadStoreProgre
     }
 }
 
+/*
+- (void)customizeBackgroundSessionConfiguration:(nonnull NSURLSessionConfiguration *)aBackgroundSessionConfiguration
+{
+    NSMutableDictionary *aHTTPAdditionalHeadersDict = [aBackgroundSessionConfiguration.HTTPAdditionalHeaders mutableCopy];
+    if (aHTTPAdditionalHeadersDict == nil) {
+        aHTTPAdditionalHeadersDict = [[NSMutableDictionary alloc] init];
+    }
+    [aHTTPAdditionalHeadersDict setObject:@"identity" forKey:@"Accept-Encoding"];
+    aBackgroundSessionConfiguration.HTTPAdditionalHeaders = aHTTPAdditionalHeadersDict;
+}
+*/
 
 #pragma mark - NSProgress
 
