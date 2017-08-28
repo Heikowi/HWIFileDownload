3ada7a327aa21f53b10581e2c4d33b1154f7e4ca
Heiko Wichmann <heiko@imagomat.de>
2017-08-28 08:09:49 +0200

    Changing mutableCopy to copy

--- Demo/HWIFileDownload/DemoDownloadStore.m
+++ Demo/HWIFileDownload/DemoDownloadStore.m
@@ -123,7 +123,7 @@ static void *DemoDownloadStoreProgressObserverContext = &DemoDownloadStoreProgre
     
     self.downloadItemsArray = [[self.downloadItemsArray sortedArrayUsingComparator:^NSComparisonResult(DemoDownloadItem*  _Nonnull aDownloadItemA, DemoDownloadItem*  _Nonnull aDownloadItemB) {
         return [aDownloadItemA.downloadIdentifier compare:aDownloadItemB.downloadIdentifier options:NSNumericSearch];
-    }] mutableCopy];
+    }] copy];
 }
 
 
